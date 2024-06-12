9e5384e7460539838fe90bbd5bc60b791803ebe3
marcosfigueiredojr <marcos.figueiredo.ti@gmail.com>
2024-06-12 16:46:18 -0300

    First Steps in Java with Spring Boot 3

---
+++ 05_FirstStepInJavawithSpringBoot/rest-with-spring-boot-and-java-marcos-1/src/main/java/br/com/marcos/Starttup.java
@@ -0,0 +1,13 @@
+package br.com.marcos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Starttup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Starttup.class, args);
+	}
+
+}
